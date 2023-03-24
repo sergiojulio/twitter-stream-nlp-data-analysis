efd3929e2e8397f6c01bd64b3c137e090610b81b
Sergio Sierra <sergiojulio@outlook.com>
2023-03-24 20:25:10 -0300

    Test2

---
+++ src/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+from twitter.twitterapi import Twitterapi
+
+
+# dev
+dotenv_path = Path('.venv')
+load_dotenv(dotenv_path=dotenv_path)
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    # init twitter
+    bearer_token = os.getenv('BEARER_TOKEN')
+    twitter = Twitterapi(bearer_token)
+    # twitter.stream("saime")
+
+
+    # init kafka
+    return {"message": twitter.test("Hello World")}
+
+
+@app.get("/token")
+async def root():
+    return {"token": os.getenv('BEARER_TOKEN')}
