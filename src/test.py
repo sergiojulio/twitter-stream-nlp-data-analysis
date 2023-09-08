54d6db0ea3d2a59169ba06c35258c7c20b79d0fc
sergiojulio <sergiojulio@gmail.com>
2023-09-08 20:57:01 -0300

    Changes

--- src/test.py
+++ src/test.py
@@ -72,11 +72,15 @@ print(printer.get_rules())
 
 """
 client.filter(
-expansions="attachments.poll_ids,attachments.media_keys,author_id,geo.place_id,in_reply_to_user_id,referenced_tweets.id,entities.mentions.username,referenced_tweets.id.author_id",
-tweet_fields="attachments,author_id,context_annotations,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang,possibly_sensitive,public_metrics,referenced_tweets,reply_settings,source,text,withheld,edit_history_tweet_ids,edit_controls",
+expansions="attachments.poll_ids,attachments.media_keys,author_id,geo.place_id,in_reply_to_user_id,
+referenced_tweets.id,entities.mentions.username,referenced_tweets.id.author_id",
+tweet_fields="attachments,author_id,context_annotations,conversation_id,created_at,entities,geo,id,
+in_reply_to_user_id,lang,possibly_sensitive,public_metrics,referenced_tweets,reply_settings,source,
+text,withheld,edit_history_tweet_ids,edit_controls",
 poll_fields="duration_minutes,end_datetime,id,options,voting_status",
 place_fields="contained_within,country,country_code,full_name,geo,id,name,place_type",
-user_fields="created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld",
+user_fields="created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,
+protected,public_metrics,url,username,verified,withheld",
 media_fields="duration_ms,height,media_key,preview_image_url,public_metrics,type,url,width"
 )
 
