ba83e2bb6d0c03be1cb564fcab8b3fd26221084d
sergiojulio <sergiojulio@gmail.com>
2024-03-27 07:45:36 -0300

    streamlit real time

--- src/app/streamlit/main.py
+++ src/app/streamlit/main.py
@@ -12,15 +12,22 @@ st.set_page_config(
 # Initialize connection.
 conn = st.connection("postgresql", type="sql")
 
-# Perform query.
-df = conn.query('SELECT * FROM stream ORDER BY tweet_created ASC LIMIT 10;', ttl="1s") # ttl for caching
+
 
 placeholder = st.empty()
 
-with placeholder.container():
-    #st.line_chart(data=None, *, x=None, y=None, color=None, width=0, height=0, use_container_width=True)
-    st.line_chart(data=df, x='tweet_created', y='polarity')
+# while or for statement
+# near real-time / live feed simulation
+# while True:
+for seconds in range(200):
 
-    st.markdown("### Detailed Data View")
-    st.dataframe(df)
-    time.sleep(1)
+    # Perform query.
+    df = conn.query('SELECT * FROM stream ORDER BY tweet_created ASC LIMIT 10;', ttl="1s") # ttl for caching
+
+    with placeholder.container():
+        #st.line_chart(data=None, *, x=None, y=None, color=None, width=0, height=0, use_container_width=True)
+        st.line_chart(data=df, x='tweet_created', y='polarity')
+
+        st.markdown("### Detailed Data View")
+        st.dataframe(df)
+        time.sleep(1)
