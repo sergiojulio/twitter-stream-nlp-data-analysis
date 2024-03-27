145b75c9cf25136597f9b5f7ce115edb48be3645
sergiojulio <sergiojulio@gmail.com>
2024-03-26 21:04:20 -0300

    streamlit changes

--- src/app/streamlit/main.py
+++ src/app/streamlit/main.py
@@ -1,12 +1,26 @@
  
 import streamlit as st
+import time 
+
+
+st.set_page_config(
+    page_title="Real-Time Data Science Dashboard",
+    page_icon="✅",
+    layout="wide",
+)
 
 # Initialize connection.
 conn = st.connection("postgresql", type="sql")
 
 # Perform query.
-df = conn.query('SELECT * FROM stream;', ttl="10m") # ttl for caching
+df = conn.query('SELECT * FROM stream ORDER BY tweet_created ASC LIMIT 10;', ttl="1s") # ttl for caching
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.text} has a :{row.polarity}:")
+placeholder = st.empty()
+
+with placeholder.container():
+    #st.line_chart(data=None, *, x=None, y=None, color=None, width=0, height=0, use_container_width=True)
+    st.line_chart(data=df, x='tweet_created', y='polarity')
+
+    st.markdown("### Detailed Data View")
+    st.dataframe(df)
+    time.sleep(1)
